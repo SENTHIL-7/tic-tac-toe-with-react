ec86db9d19d1dad32ec627a2d39484143f05d586
SENTHIL-7 <apsenthilkumar2001@gmail.com>
2024-08-15 21:57:00 +0530

    initial commit

---
+++ src/component/Board.jsx
@@ -0,0 +1,31 @@
+import './Board.css'
+import {useState} from 'react'
+
+export default function Board() {
+    return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>)
+  }
+
+  function Square(){
+    const [value , setValue] =useState(null)
+    function handleClick(){ 
+      setValue('x')
+    }
+    return <button onClick={handleClick} className="square">{value}</button>
+  }
